eab19bfa06b5420fc1faef62c7d8ada9e451a041
Mauro Tony <57079165+MauroTony@users.noreply.github.com>
2022-08-05 16:17:31 -0300

    Atualizando Urls

--- banco_api/urls.py
+++ banco_api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
 from .views import (
-    HelloWorld,
+    ClienteCreateAPIView,
+    transacaoAPIView,
+    VerificaClienteAPIView,
+    ClienteExtratoAPIView,
+    FaturaVisualizacaoAPIView,
+    FaturaPagamentoAPIView,
 )
 
 urlpatterns = [
-    path('api', HelloWorld.as_view()),
+    path("cliente", ClienteCreateAPIView.as_view()),
+    path("verifica_cliente", VerificaClienteAPIView.as_view()),
+    path("transacao", transacaoAPIView.as_view()),
+    path("extrato", ClienteExtratoAPIView.as_view()),
+    path("visualizar_fatura", FaturaVisualizacaoAPIView.as_view()),
+    path("pagamento_fatura", FaturaPagamentoAPIView.as_view()),
 ]
